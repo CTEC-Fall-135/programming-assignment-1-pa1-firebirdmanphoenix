461dc942dfc2835e8eddf54371555f83bd4e1df4
firebirdmanphoenix <phoenixtxv@gmail.com>
2025-10-01 14:37:00 -0700

    Finished PA1

---
+++ Module1/WorkflowDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ *Author: Phoenix Vu
+ *Date: 10/1/2025
+ *Assignment: PA1
+*/
+
+
+
+namespace WorkflowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Connection terminated. I'm sorry to interrupt you Elizabeth," +
+                " if you even still remember that name.");
+        }
+    }
+}
